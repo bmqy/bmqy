b928e48c6da0f48799fa151e4549662aafb2984a
bmqy <qq88268459@126.com>
2022-02-01 17:09:22 +0800

    update

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
     # 替换 ---start--- 到 ---end--- 之间的内容
     # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
-    insert_info = "<!--START_SECTION:bmqy-->\n\n### 北门清燕的博客(" + "更新时间:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') + " | 通过Github Actions自动更新~~)" +"\n" + insert_info + "\n<!--END_SECTION:bmqy-->"
+    insert_info = "<!--START_SECTION:bmqy-->\n\n### 北门清燕的博客(" + "刷新时间:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') + " | 通过Github Actions自动更新~~)" +"\n" + insert_info + "\n<!--END_SECTION:bmqy-->"
     # 获取README.md内容
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
